b4585c8bb1ec61ebd348b0f29e5402861314d409
heitorep <heitor.prudente@sou.inteli.edu.br>
2023-06-20 14:38:16 -0300

    organização script

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ const searchInput = document.getElementById('inputPesquisa');
 const searchBtn = document.getElementById('botaoPesquisa');
 var darkMode = false;
 
+/*Dark Mode */
 $(document).ready(function() {
     // Verifica se o modo escuro está ativado ou não
     darkMode = localStorage.getItem("darkMode")  === 'true'; 
@@ -68,3 +69,39 @@ function funcaoDarkMode() {
     }
     
 }
+
+/* Abre a overlay do menu lateral */
+function abrirNav() {
+    document.getElementById("menuLateral").style.width = "250px";
+}
+
+/* Fecha o overlay do menu lateral */
+function fecharNav() {
+    document.getElementById("menuLateral").style.width = "0";
+}
+
+/* Popup Sugerir Alteração (página de metadados) */
+const button = document.querySelector('.btnLapis')
+const popup = document.querySelector('.popup-wrapper')
+
+button.addEventListener('click', () => {
+    popup.style.display = 'block'
+})
+
+popup.addEventListener('click', event => {
+    const classNameOfClickedElement = event.target.classList[0]
+
+    if (classNameOfClickedElement === 'popup-close' || classNameOfClickedElement === 'popup-link' || classNameOfClickedElement === 'popup-wrapper') {
+        popup.style.display = 'none'
+    }
+    
+})
+
+/* Alerta ao enviar Sugestão de Alteração (página de metadados) */
+$('form[name="form1"]').submit(function(e){
+    e.preventDefault(); // Evita que o formulário seja enviado da maneira padrão
+    $.post($(this).attr('action'), $(this).serialize(), function(data){
+      // Aqui você pode lidar com a resposta do servidor após o POST
+      alert('Alteração sugerida com sucesso!');
+    });
+  });
